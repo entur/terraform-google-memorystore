2757dcc26343204ef0aa4ea92aadeece85633c73
Espen Blikra <espen.blikra@entur.org>
2022-07-01 14:47:25 +0200

    feat: add initial memorystore redis module

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-Init commit
+# Terraform module(s) for creating Google Cloud Memorystore instanses following Entur's conventions
+
+Modules for provisioning memorystore instances on Google Cloud Platform.
+
+## Redis module
+
+A Redis module that uses the [init module](https://github.com/entur/terraform-google-init) as minimum input, while allowing overrides and additional configuration.
+
+[Module](modules/redis)
+
+[Examples](examples)
+
+## Usage instructions
+
+### Version constraints
+
+You can control the version of a module dependency by adding `?ref=TAG` at the end of the source argument. This is highly recommended. You can find a list of available versions [here](https://github.com/entur/terraform-google-memorystore/releases).
+
+```
+module "redis" {
+  source = "github.com/entur/terraform-google-memorystore//modules/redis?ref=vVERSION"
+  ...
+}
+```
+
+Dependency automation tools such as Renovate Bot will be able to discover new releases and suggest updates automatically.
+
+#### Example
+
+```
+module "redis" {
+  source = "github.com/entur/terraform-google-memorystore//modules/redis?ref=v1.0.0"
+  ...
+}
+```
---
+++ modules/redis/main.tf
@@ -0,0 +1,52 @@
+locals {
+  generation         = format("%03d", var.generation)
+  connect_mode       = "PRIVATE_SERVICE_ACCESS"
+  tier               = "STANDARD_HA"
+  read_replicas_mode = "READ_REPLICAS_DISABLED"
+}
+
+resource "google_redis_instance" "main" {
+  name           = "mem-${var.init.app.id}-${var.init.environment}-${local.generation}"
+  project        = var.init.app.project_id
+  tier           = local.tier
+  memory_size_gb = var.memory_size_gb
+
+  region = var.region
+
+  connect_mode = local.connect_mode
+
+  redis_version = var.redis_version
+  redis_configs = var.redis_configs
+
+  authorized_network = var.init.networks.vpc_id
+  read_replicas_mode = local.read_replicas_mode
+
+  labels = var.init.labels
+
+  maintenance_policy {
+    weekly_maintenance_window {
+      day = var.maintenance_window.day
+      start_time {
+        hours   = var.maintenance_window.hour
+        minutes = 0
+        seconds = 0
+        nanos   = 0
+      }
+    }
+
+  }
+}
+
+
+resource "kubernetes_config_map" "main_redis_connection" {
+  metadata {
+    name      = "${var.init.app.name}-redis-connection"
+    namespace = var.init.app.name
+    labels    = var.init.labels
+  }
+
+  data = {
+    REDIS_HOST = google_redis_instance.main.host
+    REDIS_PORT = google_redis_instance.main.port
+  }
+}
