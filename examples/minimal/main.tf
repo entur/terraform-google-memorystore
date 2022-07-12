cdacddcd16e8d1670ae035c4930d5cee687ceb4c
Espen Blikra <Spikra@users.noreply.github.com>
2022-07-12 09:41:37 +0200

    docs: update docs and example to use release-please version bump (#5)

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -2,16 +2,14 @@ module "init" {
   # This is an example only; if you're adding this block to a live configuration,
   # make sure to use the latest release of the init module, found here:
   # https://github.com/entur/terraform-google-init/releases
-  source      = "github.com/entur/terraform-google-init//modules/init?ref=v1.1.0"
+  source      = "github.com/entur/terraform-google-init//modules/init?ref=v0.2.1"
   app_id      = "tfmodules"
   environment = "dev"
 }
 
+# ci: x-release-please-start-version
 module "redis" {
-  # This is for local reference only; if you're using this module as a published
-  # module from GitHub, the 'source' parameter must refer to it's public location.
-  # See README.md for instructions.
-  # source     = "github.com/entur/terraform-google-memorystore//modules/redis?ref=vVERSION"
-  source     = "../../modules/redis"
-  init       = module.init
+  source = "github.com/entur/terraform-google-memorystore//modules/redis?ref=v0.1.0"
+  init   = module.init
 }
+# ci: x-release-please-end
