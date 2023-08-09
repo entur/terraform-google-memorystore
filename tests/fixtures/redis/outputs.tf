583ecd8ca0b354386e8eda693d2e59f586c29c22
Bjørn Vestli <33520068+bvestli@users.noreply.github.com>
2023-08-09 10:02:43 +0200

    test: Add integration test (#22)
    * test: Add integration test
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: Bjørn Vestli <bjorn.vestli@entur.org>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tests/fixtures/redis/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "instance" {
-  value       = module.redis.instance
-  sensitive   = true
-  description = "The redis instance output, as described in https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/redis_instance."
-}
