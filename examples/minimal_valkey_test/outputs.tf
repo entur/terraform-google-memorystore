c457d85cf5bc1e235bf166330eeec6aaf88ae350
Marta Paciorkowska <marta.paciorkowska@entur.org>
2026-04-15 12:52:31 +0200

    Valkey: updates test arguments

--- examples/minimal_valkey_test/outputs.tf
+++ examples/minimal_valkey_test/outputs.tf
@@ -7,3 +7,8 @@ output "project_id" {
   description = "Project ID"
   value       = module.init.app.project_id
 }
+
+output "instance" {
+  description = "The memorystore instance output, as described in https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/memorystore_instance."
+  value       = module.valkey.instance
+}
